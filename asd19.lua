b29aa3acca02297a37ada7a2f3982ba10aa17bab
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2025-06-30 21:10:46 +0700

    Update asd19.lua

--- asd19.lua
+++ asd19.lua
@@ -3,7 +3,7 @@ if not game:IsLoaded() then
 end
 function getpot()
     for _,v in pairs(workspace.Plots:GetChildren()) do
-        if string.find(v.PlotSign.SurfaceGui.Frame.TextLabel.Text, game.Players.LocalPlayer.Name) then
+        if string.find(v.PlotSign.SurfaceGui.Frame.TextLabel.Text, game.Players.LocalPlayer.DisplayName) then
             return v
         end
     end
